d58d80960fe8a7100749eff5a9e705edb921bdc2
g1627007 <g1627007@st.t-kougei.ac.jp>
2017-06-26 17:49:36 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -3,11 +3,19 @@ document.getElementById('from').onsubmit = function(){
 };
 
 for(var i = 1; i < 101; i++){
+
+  if(i%3 = 0){
+    li.textContent = Fizz;
+  }
+  else if(i%5 = 0){
+    li.textContent = Buzz;
+  }
+  else if(i%15 = 0){
+    li.textContent = FizzBuzz;
+  }
+  else(){
+    li.textContent = i;
+  }
   
-  var li =document.createElement('li');
-  li.textContent = i;
-  document.getElementById('list').appendChild(li);
-
-
 }
 
