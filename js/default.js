a305a02c302157307e08aaaacd9e6e9f886b4e37
g1627007 <g1627007@st.t-kougei.ac.jp>
2017-06-26 18:08:46 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,13 +1,9 @@
-document.getElementById('from').onsubmit = function(){
-  window.alert(document.getElementById('from').word.value);
-};
-
-
 for(var i = 1; i < 101; i++){
     var li =document.createElement('li');
   li.textContent = i;
   document.getElementById('list').appendChild(li);
 
 }
-
- 
+document.getElementById('from').onsubmit = function(){
+  window.alert(document.getElementById('from').word.value);
+}
