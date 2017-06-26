a5d2a2cd7a824d4996322393535354dac9f887b6
g1627007 <g1627007@st.t-kougei.ac.jp>
2017-06-26 17:27:18 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -4,17 +4,5 @@ for(var i = 1; i < 101; i++){
   li.textContent = i;
   document.getElementById('list').appendChild(li);
   
-  if(i%3 = 0){
-    li.textContent = Fizz;
-  }
-  else if(i%5 = 0){
-    li.textContent = Buzz;
-  }
-  else if(i%15 = 0){
-    li.textContent = FizzBuzz;
-  }
-  else(){
-    li.textContent = i;
-  }
 
 }
