d76b42c3e7aa35ab7f0b228f314924f58f0b3fc1
g1627007 <g1627007@st.t-kougei.ac.jp>
2017-06-26 17:26:10 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,6 +1,7 @@
 for(var i = 1; i < 101; i++){
   
   var li =document.createElement('li');
+  li.textContent = i;
   document.getElementById('list').appendChild(li);
   
   if(i%3 = 0){
@@ -15,4 +16,5 @@ for(var i = 1; i < 101; i++){
   else(){
     li.textContent = i;
   }
+
 }
