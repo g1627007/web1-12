10c9f71ed6da75118699dc6e64ef738ffe06a82f
g1627007 <g1627007@st.t-kougei.ac.jp>
2017-06-26 18:02:36 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,24 +1,10 @@
 
-public class fizzbuzz{
-  public static void fizzbuzz(String[] args){
-  List<String> list = new ArrayList<String>();
-  var li =document.createElement('li');
+
+for(var i = 1; i < 101; i++){
+    var li =document.createElement('li');
   li.textContent = i;
   document.getElementById('list').appendChild(li);
-for(var i = 1; i < 101; i++){
-  if(i%3 = 0){
-    li.textContent = Fizz;
-  }
-  else if(i%5 = 0){
-    li.textContent = Buzz;
-  }
-  else if(i%15 = 0){
-    li.textContent = FizzBuzz;
-  }
-  else(){
-    li.textContent = i;
-  }
-  
+
 }
 
 
